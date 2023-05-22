fd9ca0c2af00e9f291edcc20bb6630691f93f8f0
YJ <yjimenez@iesnervion.es>
2023-05-22 14:37:52 +0200

    Ejercicio 4 terminado

--- src/ejercicios/Ejercicio4.java
+++ src/ejercicios/Ejercicio4.java
@@ -9,12 +9,16 @@ public class Ejercicio4 {
         TreeMap<Integer, Integer> numeros = new TreeMap<>();
         // Creación de una lista
         TreeMap<Integer, Integer> estrellas = new TreeMap<>();
+        // Atributo para guardar la clave como objeto
+        Object clave;
+        // Atributo donde guardamos el número en tipo int
+        int codigo;
+
+        // Atributo número para introducir por el usuario
+        int num;
 
         // Creación de escáner
         Scanner key = new Scanner(System.in);
-
-        // Atributo número para introducir
-        int num;
 
         // Atributo que comprueba si quiere seguir metiendo números
         String seguir;
@@ -22,16 +26,48 @@ public class Ejercicio4 {
         do {
             // Introduce los números
             for (int i = 0; i < 5; i++) {
+                // Introduce el número
+                System.out.println("Mete un número: ");
                 num = key.nextInt();
                 key.nextLine();
-                if (numeros.) {
+                // Inserta el número con el put y lo guarda en la clave de tipo object
+                clave = numeros.put(num, 1);
 
+                // Comprueba si la clave es distinta de null
+                if (clave != null) {
+                    // Hace un cast a la clave y la guarda en otra variable
+                    codigo = (int) clave;
+                    // Reemplaza el código más uno
+                    numeros.replace(num, codigo + 1);
                 }
-                numeros.put(num, 1);
             }
 
+            // Introduce los números
+            for (int i = 0; i < 2; i++) {
+
+                    System.out.println("Mete una estrella: ");
+                    num = key.nextInt();
+                    key.nextLine();
+
+                clave = estrellas.put(num, 1);
+
+                if (clave != null) {
+                    codigo = (int) clave;
+                    estrellas.replace(num, codigo + 1);
+                }
+            }
+
+            // Imprime las estrellas y números
+            System.out.println("---Números---");
+            System.out.println(numeros);
+            System.out.println("\n---Estrellas---");
+            System.out.println(estrellas);
+
+            System.out.println("¿Quieres seguir?: ");
             seguir = key.nextLine();
-        } while (seguir.equals("Si") || seguir.equals("si"));
+        } while (seguir.equalsIgnoreCase("Si"));
+
+
 
     }
 }
